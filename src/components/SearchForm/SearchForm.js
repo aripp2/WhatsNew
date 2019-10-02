d550197e6634697f2a510234bd1e817d945776da
Amy Rippeto <amyrippeto@gmail.com>
2019-10-02 17:37:23 -0600

    Set up test files

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-const SearchForm = () => {
+class SearchForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: ''
+    }
+  }
+
+render() {
   return (
     <form>
-      <input 
+      <input
+        className="search-input" 
         type="text"
         placeholder="Search for news article here."
+        name="searchInput"
+        
       />
       <button className="search-btn">Search Now</button>
     </form>
 
-    )
+      )
+  }
 }
 
 export default SearchForm;
