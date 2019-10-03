2ba30519600ec2d363eeb91f4104b0e41c05a0fa
Amy Rippeto <amyrippeto@gmail.com>
2019-10-02 18:02:44 -0600

    Pass snapshot NewsArticle test

---
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NewsArticle from './NewsArticle';
+
+describe('NewsArticle', () => {
+  it('should match the snapshot with all data passed in correctly', () => {
+    const wrapper = shallow(<NewsArticle
+      key={2}
+      image="url"
+      headline="This is the headline"
+      description="This is the description"
+      url="link"
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
---
+++ src/components/NewsContainer/NewsContainer.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NewsContainer from './NewsContainer';
+
+describe('NewsContainer', () => {
+  it('should match the snapshot with all data passed in correctly', () => {
+    const wrapper = shallow(<NewsContainer 
+      {articles}
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
