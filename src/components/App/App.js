5b98c6a06426ccddc440f91e939977139dba94c5
Amy Rippeto <amyrippeto@gmail.com>
2019-10-04 20:05:42 -0600

    Remove fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,45 +1,33 @@
 import React, { Component } from 'react';
-// import local from '../../data/local';
-// import entertainment from '../../data/entertainment';
-// import health from '../../data/health';
-// import science from '../../data/science';
-// import technology from '../../data/technology';
 import Menu from '../Menu/Menu';
 import SearchForm from '../SearchForm/SearchForm';
 import NewsContainer from '../NewsContainer/NewsContainer';
 import './App.css';
 
+import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      news: {},
-      currentNews: []
-    //   news: {
-    //     local,
-    //     entertainment,
-    //     health,
-    //     science,
-    //     technology
-    //   },
-    //   currentNews: local
+      news: {
+        local,
+        entertainment,
+        health,
+        science,
+        technology
+      },
+      currentNews: local
     }
   }
 
   changeNewsType = (newsType) => {
     this.setState({ currentNews: this.state.news[newsType]})
   }
-
-  componentDidMount() {
-    fetch("https://whats-new-api.herokuapp.com/api/v1/news")
-      .then(response => response.json())
-      .then(data => this.setState({ 
-        news: data,
-        currentNews: data.local
-      }))
-      .catch(err => console.log(err))
-  }
-
 
   searchNews = (input) => {
     const fixedInput = input.toUpperCase();
@@ -48,7 +36,6 @@ class App extends Component {
       const fixedDescription = article.description.toUpperCase();
       return fixedHeadline.includes(fixedInput) || fixedDescription.includes(fixedInput)
     })
-
     this.setState({ currentNews: matches });
   }
 
