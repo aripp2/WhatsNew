92fc835032b0db7d851b29d0899bd3a636da8620
Amy Rippeto <amyrippeto@gmail.com>
2019-10-04 20:18:41 -0600

    Remove data imports and update snapshot for app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,4 @@
 import React, { Component } from 'react';
-// import local from '../../data/local';
-// import entertainment from '../../data/entertainment';
-// import health from '../../data/health';
-// import science from '../../data/science';
-// import technology from '../../data/technology';
 import Menu from '../Menu/Menu';
 import SearchForm from '../SearchForm/SearchForm';
 import NewsContainer from '../NewsContainer/NewsContainer';
@@ -15,14 +10,6 @@ class App extends Component {
     this.state = {
       news: {},
       currentNews: []
-    //   news: {
-    //     local,
-    //     entertainment,
-    //     health,
-    //     science,
-    //     technology
-    //   },
-    //   currentNews: local
     }
   }
 
@@ -39,7 +26,6 @@ class App extends Component {
       }))
       .catch(err => console.log(err))
   }
-
 
   searchNews = (input) => {
     const fixedInput = input.toUpperCase();
