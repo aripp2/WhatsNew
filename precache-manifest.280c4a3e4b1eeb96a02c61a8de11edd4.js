7b41070b771eb3ee6ca80b5bdc438ab8fcd6ecae
Amy Rippeto <amyrippeto@gmail.com>
2019-10-06 16:32:47 -0600

    Updates

---
+++ precache-manifest.280c4a3e4b1eeb96a02c61a8de11edd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f48118ec5ac11e5d104ffc4cf8520c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a36e2c9659df0793169",
+    "url": "./static/css/main.666ed9c6.chunk.css"
+  },
+  {
+    "revision": "32736601840bcfe871af",
+    "url": "./static/js/2.b47c86c1.chunk.js"
+  },
+  {
+    "revision": "7a36e2c9659df0793169",
+    "url": "./static/js/main.3ec11652.chunk.js"
+  },
+  {
+    "revision": "a7ba3a89543cc5257511",
+    "url": "./static/js/runtime-main.bf980d17.js"
+  }
+]);
